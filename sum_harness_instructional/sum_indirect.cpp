1bcd61006ae788bc51ce7f3754b61ac7d7b12406
tokoHerold <wombatwheelie@gmail.com>
2025-08-31 16:43:53 -0700

    Implement indirect sum

--- sum_harness_instructional/sum_indirect.cpp
+++ sum_harness_instructional/sum_indirect.cpp
@@ -1,24 +1,29 @@
 #include <algorithm>
 #include <chrono>
+#include <cstdlib>
 #include <iomanip>
 #include <iostream>
 #include <random>
-#include <vector>
+#include <ratio>
 #include <string.h>
+#include <vector>
 
 #include "sums.h"
 
-void 
-setup(int64_t N, float A[])
-{
-   printf(" inside sum_indirect problem_setup, N=%ld \n", N);
+void setup(int64_t N, float A[]) {
+    printf(" inside sum_indirect problem_setup, N=%ld \n", N);
+    for (int i = 0; i < N; ++i) {
+        A[i] = (float)(lrand48() % N);
+    }
 }
 
-float
-sum(int64_t N, float A[])
-{
-   printf(" inside sum_indirect perform_sum, N=%ld \n", N);
-
-   return 0;
+float sum(int64_t N, float A[]) {
+    int64_t next = N - 1; // Prevent libc call
+    float sum = 0;
+    for (int i = 0; i < N; ++i) {
+        float tmp = A[next];
+        sum += tmp;
+        next = (int)tmp;
+    }
+    return sum;
 }
-
