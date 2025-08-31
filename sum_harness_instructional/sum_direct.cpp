c6dbaa251e0d67f9e1fa09c890f88b2f27eff1d1
tokoHerold <wombatwheelie@gmail.com>
2025-08-31 15:58:40 -0700

    Standard sum implementation

--- sum_harness_instructional/sum_direct.cpp
+++ sum_harness_instructional/sum_direct.cpp
@@ -3,22 +3,21 @@
 #include <iomanip>
 #include <iostream>
 #include <random>
-#include <vector>
 #include <string.h>
+#include <vector>
 
 #include "sums.h"
 
-void 
-setup(int64_t N, float A[])
-{
-   printf(" inside direct_sum problem_setup, N=%ld \n", N);
+void setup(int64_t N, float A[]) {
+    printf(" inside direct_sum problem_setup, N=%ld \n", N);
 }
 
-float
-sum(int64_t N, float A[])
-{
-   printf(" inside direct_sum perform_sum, N=%ld \n", N);
-
-   return 0;
+float sum(int64_t N, float A[]) {
+    // Remove printf statement
+    // printf(" inside direct_sum perform_sum, N=%ld \n", N);
+	float sum = 0;
+	for (int i = 0; i < N; ++i) {
+		sum += i;
+	}
+    return sum;
 }
-
