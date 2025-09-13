443829c4057f341c3780a06e0c96aaa0ec88da29
tokoHerold <wombatwheelie@gmail.com>
2025-09-12 22:30:24 -0700

    Implement basic matrix multiplication

--- mmul-harness-instructional/dgemm-basic.cpp
+++ mmul-harness-instructional/dgemm-basic.cpp
@@ -6,7 +6,15 @@ const char* dgemm_desc = "Basic implementation, three-loop dgemm.";
  * where A, B, and C are n-by-n matrices stored in row-major format.
  * On exit, A and B maintain their input values.
  */
-void square_dgemm(int n, double* A, double* B, double* C) 
-{
-   // insert your code here: implementation of basic matrix multiple
+void square_dgemm(int n, double* A, double* B, double* C) {
+	for (int row = 0; row < n; ++row) {
+		for (int col = 0; col < n; ++col) {
+			int idx_c = row * n + col;
+			for (int i = 0; i < n; ++i) {
+				int idx_a = row * n + i;
+				int idx_b = i * n + col;
+				C[idx_c] += A[idx_a] * B[idx_b];
+			}
+		}
+	}
 }
