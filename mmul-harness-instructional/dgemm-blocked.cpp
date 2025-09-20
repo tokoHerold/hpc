669246b6c2a9c83f26bc4595e18bb44f2ade972b
tokoHerold <wombatwheelie@gmail.com>
2025-09-19 23:20:14 -0700

    Only use square tiles instead of rows/columns

--- mmul-harness-instructional/dgemm-blocked.cpp
+++ mmul-harness-instructional/dgemm-blocked.cpp
@@ -5,35 +5,34 @@ const char* dgemm_desc = "Blocked dgemm.";
  *  C := C + A * B
  * where A, B, and C are n-by-n matrices stored in row-major format.
  * On exit, A and B maintain their input values. */
-// void square_dgemm_blocked(int n, int block_size, double* __restrict A, double* __restrict B, double* __restrict C) {
 void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C) {
-	double* row_tile_buffer = new double[n * block_size];
-	double* col_tile_buffer = new double[n * block_size];
+	double* row_tile_buffer = new double[block_size * block_size];
+	double* col_tile_buffer = new double[block_size  * block_size];
 	double* tile_buffer = new double[block_size * block_size];
 
 	for (int block_i = 0; block_i < n; block_i += block_size) {
-		// Copy optimization: Load A[block_i:block_i+block_size, 0:n] into buffer
-		memcpy(row_tile_buffer, &A[block_i * n], (size_t) block_size * n * sizeof(double));
 
 		for (int block_j = 0; block_j < n; block_j += block_size) {
-			// Copy optimization: Load B[0:n, block_j:block_j+block_size] into buffer
-			for (int row = 0, dst_offset = 0; row < n; ++row, dst_offset += block_size) {
-				memcpy(&col_tile_buffer[row * block_size], &B[row * n + block_j], block_size * sizeof(double));
-			}
 			// Copy optimization: Load C[block_i:block_i+block_size, block_j:block_j+block_size] into buffer
 			for (int row = 0; row < block_size; ++row) {
 				memcpy(&tile_buffer[row * block_size], &C[(block_i + row) * n + block_j], block_size * sizeof(double));
 			}
 
 			for (int block_k = 0; block_k < n; block_k += block_size) {
+				// Copy optimization: Load A[block_i:block_i+block_size, block_k:block_k+block_size] into buffer
+				// Copy optimization: Load B[block_k:block_k+block_size, block_j:block_j+block_size] into buffer
+				for (int row = 0; row < block_size; ++row) {
+					memcpy(&row_tile_buffer[row * block_size], &A[(block_i + row) * n + block_k], block_size * sizeof(double));
+					memcpy(&col_tile_buffer[row * block_size], &B[(block_k + row) * n + block_j], block_size * sizeof(double));
+				}
 
-				// Perform matrix multiplication on every element inside the block
+				// Perform matrix multiplication on every element inside the tile
 				for (int i = 0; i < block_size; ++i) {
 					for (int j = 0; j < block_size; ++j) {
 						int idx_c = i * block_size + j;
 						for (int k = 0; k < block_size; ++k) {
-							int idx_a = i * n + (block_k + k);
-							int idx_b = (block_k + k) * block_size + j;
+							int idx_a = i * block_size + k;
+							int idx_b = k * block_size + j;
 							tile_buffer[idx_c] += row_tile_buffer[idx_a] * col_tile_buffer[idx_b];
 						}
 					}
