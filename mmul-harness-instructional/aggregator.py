9f753da1a096960d4ec321da144c6f0251b6987b
tokoHerold <wombatwheelie@gmail.com>
2025-09-23 17:35:12 -0700

    Add missing multiplicator

--- mmul-harness-instructional/aggregator.py
+++ mmul-harness-instructional/aggregator.py
@@ -7,8 +7,8 @@ input_dfs = [pd.read_csv(f"data/{problem}.csv") for problem in problems]
 basic_blas = pd.DataFrame()
 basic_blas["N"] = input_dfs[0]["N"]
 
-basic_blas["basic"] = input_dfs[0]["N"]**3 / input_dfs[0]["runtime"] / 10**6
-basic_blas["blas"] = input_dfs[2]["N"]**3 / input_dfs[2]["runtime"] / 10**6
+basic_blas["basic"] = 2 * input_dfs[0]["N"]**3 / input_dfs[0]["runtime"] / 10**6
+basic_blas["blas"] = 2 * input_dfs[2]["N"]**3 / input_dfs[2]["runtime"] / 10**6
 
 basic_blas.to_csv("data/mflops_basic.csv", index=False)
 
@@ -18,8 +18,8 @@ blocked_blas["N"] = input_dfs[0]["N"]
 vars = input_dfs[1].groupby("block_size")
 for key in vars.groups.keys():
     group = vars.get_group(key)
-    values = (group["N"]**3 / group["runtime"] / 10**6).reset_index(drop=True)
+    values = (2 * group["N"]**3 / group["runtime"] / 10**6).reset_index(drop=True)
     blocked_blas[f"b = {key}"] = values
 
-blocked_blas["blas"] = input_dfs[2]["N"]**3 / input_dfs[2]["runtime"] / 10**6
+blocked_blas["blas"] = 2 * input_dfs[2]["N"]**3 / input_dfs[2]["runtime"] / 10**6
 blocked_blas.to_csv("data/mflops_blocked.csv", index=False)
