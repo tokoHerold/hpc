17f1337bfdf71a0646e72164dd388f95fc051037
tokoHerold <wombatwheelie@gmail.com>
2025-10-02 15:26:49 -0700

    Add aggregator script

--- vmmul-omp-harness-instructional/aggregator.py
+++ vmmul-omp-harness-instructional/aggregator.py
@@ -16,18 +16,28 @@ serial["blas"] = n_to_flops(input_dfs[2]["N"]) / input_dfs[2]["t"] / 10**6
 serial.to_csv("data/mflops_serial.csv", index=False)
 
 
-# Compare parallel to blas
+# Compare best parallel to blas
+best_idx = 5
 parallel = pd.DataFrame()
 parallel["N"] = input_dfs[0]["N"]
 parallel["blas"] = n_to_flops(input_dfs[2]["N"]) / input_dfs[2]["t"] / 10**6
-for i in range(3, 7):
-    parallel[problems[i]] = n_to_flops(input_dfs[i]["N"]) / input_dfs[i]["t"] / 10**6
+parallel[problems[best_idx]] = n_to_flops(input_dfs[best_idx]["N"]) / input_dfs[best_idx]["t"] / 10**6
 parallel.to_csv("data/mflops_parallel.csv", index=False)
 
 # Calculate speedup
 speedup = pd.DataFrame()
 speedup["N"] = input_dfs[0]["N"]
 for i in range(3, 7):
-    speedup[problems[i]] = input_dfs[2]["t"] / input_dfs[i]["t"] 
+    speedup[problems[i]] = input_dfs[0]["t"] / input_dfs[i]["t"] 
 
 speedup.to_csv("data/speedup.csv", index=False)
+
+# Calculate memory bandwidth for CBLAS, Basic, OMP 1-64
+C = 204.8e9  # 204.8 GB/s
+n_to_bytes = lambda n: 2*n + 2*n**2 * 8
+bandwidth = pd.DataFrame()
+bandwidth["N"] = input_dfs[0]["N"]
+bandwidth["basic"] = n_to_bytes(input_dfs[0]["N"]) / input_dfs[0]["t"] / C
+for i in range(2, 7):
+    bandwidth[problems[i]] = (n_to_bytes(input_dfs[i]["N"]) / input_dfs[i]["t"]) / C
+bandwidth.to_csv("data/bandwidth.csv", index=False)
