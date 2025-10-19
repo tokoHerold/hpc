73fa581c26e09474eddd35f47e37e58069b596ea
tokoHerold <wombatwheelie@gmail.com>
2025-10-19 14:00:21 -0700

    Format code

--- mmul-omp-harness-instructional/dgemm-basic-omp.cpp
+++ mmul-omp-harness-instructional/dgemm-basic-omp.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <omp.h>
+
 #include "likwid-stuff.h"
 
 const char* dgemm_desc = "Basic implementation, OpenMP-enabled, three-loop dgemm.";
@@ -10,16 +10,14 @@ const char* dgemm_desc = "Basic implementation, OpenMP-enabled, three-loop dgemm
  * where A, B, and C are n-by-n matrices stored in row-major format.
  * On exit, A and B maintain their input values.
  */
-void square_dgemm(int n, double* A, double* B, double* C) 
-{
-   // insert your code here: implementation of basic matrix multiply with OpenMP parallelism enabled
-	#pragma omp parallel
+void square_dgemm(int n, double* A, double* B, double* C) {
+	// insert your code here: implementation of basic matrix multiply with OpenMP parallelism enabled
+#pragma omp parallel
 	{
-
 #ifdef LIKWID_PERFMON
 		LIKWID_MARKER_START(MY_MARKER_REGION_NAME);
 #endif
-		#pragma omp for collapse(2)
+#pragma omp for collapse(2)
 		for (int row = 0; row < n; ++row) {
 			for (int col = 0; col < n; ++col) {
 				int idx_c = row * n + col;
@@ -35,9 +33,7 @@ void square_dgemm(int n, double* A, double* B, double* C)
 #endif
 	}
 
-   // be sure to include LIKWID_MARKER_START(MY_MARKER_REGION_NAME) inside the block of parallel code,
-   // but before your matrix multiply code, and then include LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME)
-   // after the matrix multiply code but before the end of the parallel code block.
-
-   std::cout << "Insert your basic matrix multiply, openmp-parallel edition here " << std::endl;
+	// be sure to include LIKWID_MARKER_START(MY_MARKER_REGION_NAME) inside the block of parallel code,
+	// but before your matrix multiply code, and then include LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME)
+	// after the matrix multiply code but before the end of the parallel code block.
 }
--- mmul-omp-harness-instructional/dgemm-blas.cpp
+++ mmul-omp-harness-instructional/dgemm-blas.cpp
@@ -1,5 +1,6 @@
 #include <cblas.h>
 #include <omp.h>
+
 #include "likwid-stuff.h"
 
 const char* dgemm_desc = "Reference dgemm.";
@@ -13,13 +14,11 @@ const char* dgemm_desc = "Reference dgemm.";
  * via the standard FORTRAN interface - hence the reference semantics.
  */
 void square_dgemm(int n, double* A, double* B, double* C) {
-
 #ifdef LIKWID_PERFMON
-      LIKWID_MARKER_START(MY_MARKER_REGION_NAME);
+	LIKWID_MARKER_START(MY_MARKER_REGION_NAME);
 #endif
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, n, n, n, 1., A, n, B, n, 1., C, n);
+	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, n, n, n, 1., A, n, B, n, 1., C, n);
 #ifdef LIKWID_PERFMON
-      LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME);
+	LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME);
 #endif
-
 }
