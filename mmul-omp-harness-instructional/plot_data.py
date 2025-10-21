5fd323d8260bb12abd781f6c2075713e1d912909
tokoHerold <wombatwheelie@gmail.com>
2025-10-20 17:24:31 -0700

    Finish Speedup plots

--- mmul-omp-harness-instructional/plot_data.py
+++ mmul-omp-harness-instructional/plot_data.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 
 def plot(
         title: str,
-        df: pd.DataFrame, 
+        dfs: list[pd.DataFrame] | pd.DataFrame,
         output_name: str,
         xlabel: str,
         ylabel: str,
-        legend_prefix="",
+        legend_prefix: str | list[str] = "",
         ymodifier=1.0,
         logscale="",
         log_base_x=10.0,
@@ -25,8 +25,9 @@ def plot(
 
     Args:
         title (str): The title of the plot.
-        df (pd.DataFrame): The DataFrame to plot. The index is used for the x-axis,
-                           and each column is plotted as a separate series.
+        dfs (pd.DataFrame | list[pd.DataFrame]): The DataFrame(s) to plot. The index is used for the x-axis,
+                           and each column is plotted as a separate series. If a list is passed, different
+                           DataFrames will use different line styles.
         output_name (str): The filename for the saved plot (e.g., 'my_plot.pdf').
         xlabel (str): The label for the x-axis.
         ylabel (str): The label for the y-axis.
@@ -35,21 +36,30 @@ def plot(
                                   set the corresponding axes to a log scale. Defaults to "".
         log_base_x (int, optional): The base for the logarithmic x-axis. Defaults to 2.
         log_base_y (int, optional): The base for the logarithmic y-axis. Defaults to 10.
-        legend_prefix (str, optional): A prefix to add to each legend entry. Defaults to "".
+        legend_prefix (str | list[str], optional): A prefix to add to each legend entry of each series.
+                                                    Defaults to "".
         x_cords (list[int], optional): Spacing for ticks on the x-axis.
     """
+    if type(dfs) is not list:
+        if type(dfs) is not pd.DataFrame:
+            print(f"Error: Expected dataframe but got {type()}")
+        dfs = [dfs]
+
+    if type(legend_prefix) is not list:
+        legend_prefix = [legend_prefix] * len(dfs)
+    assert len(dfs) == len(legend_prefix), "Length of data frames must be equal to legend prefixes!"
+
+    # Define cycles for visual properties
+    linestyles = ['-', '--', ':', '-.']
+    markers = ['o', 'x', '^', 's', 'D', '*', 'P', 'H']
+    colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+    base_df = dfs[0]
     print(f"-= Plotting {title.split("\n")[0]} =-")
-    print(df)
-
-    var_names = list(df.columns)
-    print("Detected series: ", [f"{legend_prefix}{x}" for x in var_names])
-
-    # Split the df into individual vars
-    # problem_sizes = df[var_names[0]].values.tolist()
-    x_labels = df.index.tolist()
+    x_labels = base_df.index.tolist()
 
     # Prepare metrics for all columns except the first one
-    metrics = [(df[var_names[i]] * ymodifier).values.tolist() for i in range(len(var_names))]
+    # metrics = [(base_df[var_names[i]] * ymodifier).values.tolist() for i in range(len(var_names))]
 
     plt.figure()
     plt.title(title)
@@ -58,12 +68,21 @@ def plot(
     plt.xticks(xlocs, [f"${problem_size}$" for problem_size in x_labels])
 
     # Define a list of markers to use
-    markers = ['o', 'x', '^', 's', 'D', '*', 'P', 'H']
-    marker_cycle = itertools.cycle(markers)
+    # marker_cycle = itertools.cycle(markers)
+    for df_idx, df in enumerate(dfs):
+        print(df)
+        linestyle = linestyles[df_idx % len(linestyles)]
+        var_names = list(df.columns)
+        print("Detected series: ", [f"{legend_prefix[df_idx]}{x}" for x in var_names])
+        print("------==========================================------")
 
-    # Plot each metric with a different marker
-    for i, metric in enumerate(metrics):
-        plt.plot(xlocs, metric, marker=next(marker_cycle), label=f"{legend_prefix}{var_names[i]}")
+        for metric_idx, var_name in enumerate(var_names):
+            metric = (df[var_name] * ymodifier).values.tolist()
+            color = colors[metric_idx % len(colors)]
+            marker = markers[metric_idx % len(markers)]
+
+            label = f"{legend_prefix[df_idx]}{var_name}"
+            plt.plot(xlocs, metric, marker=marker, color=color, linestyle=linestyle, label=label)
 
     if 'y' in logscale:
         plt.yscale("log", base=log_base_y)
@@ -81,6 +100,7 @@ def plot(
 
     # Save the figure before trying to show the plot
     plt.savefig(output_name, dpi=300, format='pdf')
+    print(f"Saved plot to {output_name}.pdf")
     # plt.show()
 
 
@@ -102,11 +122,36 @@ basic_df['Speedup'] = baseline_runtime / basic_df['Runtime (RDTSC)']
 speedup_df = basic_df.pivot(index='Problem Size', columns='Number of threads', values='Speedup')
 speedup_df.drop(columns=1, inplace=True)
 
-plot("Speedup of Basic vs. LibSci BLAS DGEMM:\nPerlmutter CPU Node, -O3, -march=native, 64-bit floats",
+plot("Speedup of Basic OMP vs Sequential DGEMM:\nPerlmutter CPU Node, -O3, -march=native, 64-bit floats",
      speedup_df,
      "basic_speedup.pdf",
      "Problem Size (n)",
      "Speedup",
      legend_prefix='p=',
+     x_coords=[0, 1, 3],
+     )
+
+
+blocked_df = flops_df[flops_df['Benchmark'] == 'blocked-omp'].copy()
+blocked_df = blocked_df.sort_values(by=['Problem Size', 'Number of threads'])
+blocked_groups = blocked_df.groupby('Number of blocks')
+df_bf4 = blocked_groups.get_group(4.0).copy()
+df_bf16 = blocked_groups.get_group(16.0).copy()
+baseline_bf4 = df_bf4[df_bf4['Number of threads'] == 1].set_index('Problem Size')['Runtime (RDTSC)']
+df_bf4['Speedup'] = df_bf4['Problem Size'].map(baseline_bf4) / df_bf4['Runtime (RDTSC)']
+speedup_bf4 = df_bf4.pivot(index='Problem Size', columns='Number of threads', values='Speedup')
+baseline_bf16 = df_bf16[df_bf16['Number of threads'] == 1].set_index('Problem Size')['Runtime (RDTSC)']
+df_bf16['Speedup'] = df_bf16['Problem Size'].map(baseline_bf16) / df_bf16['Runtime (RDTSC)']
+speedup_bf16 = df_bf16.pivot(index='Problem Size', columns='Number of threads', values='Speedup')
+# blocked_df['Speedup'] = baseline_runtime / blocked_df['Runtime (RDTSC)']
+# speedup_df = blocked_df.pivot(index='Problem Size', columns='Number of threads', values='Speedup')
+speedup_bf4.drop(columns=1, inplace=True)
+speedup_bf16.drop(columns=1, inplace=True)
+plot("Speedup of Blocked OMP vs Sequential DGEMM:\nPerlmutter CPU Node, -O3, -march=native, 64-bit floats",
+     [speedup_bf4, speedup_bf16],
+     "blocked_speedup.pdf",
+     "Problem Size (n)",
+     "Speedup",
+     legend_prefix=['b=4, p=', 'b=16, p='],
      x_coords=[0, 1, 3],
      )
